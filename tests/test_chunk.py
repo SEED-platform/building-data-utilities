734067e5af62fff9719ce34f2ddc6ec9f26911ce
Nicholas Long <1907354+nllong@users.noreply.github.com>
2025-04-11 14:01:31 -0600

    Set buildingid repo to https, add unit tests (#9)
    * use ssh to checkout repo
    
    * Change rev to branch
    
    * update precommit and buildingid url to https
    
    * add pytest
    
    * add actual test
    
    * update URLs
    
    * update readme
    
    * more formatting....
    
    * more testing
    
    ---------
    
    Co-authored-by: Alex Swindler <Alex.Swindler@nrel.gov>

---
+++ tests/test_chunk.py
@@ -0,0 +1,17 @@
+# class for holding tests with setup and teardown
+
+from utils.chunk import chunk
+
+
+class TestChunk:
+    def setup_method(self):
+        pass
+
+    def teardown_method(self):
+        pass
+
+    def test_split_list(self):
+        input_list = [1, 2, 3, 4, 5]
+        chunk_size = 2
+        expected_output = [[1, 2], [3, 4], [5]]
+        assert chunk(input_list, chunk_size) == expected_output
