2291334c597d3ca60a11b5d621ff9489463898e6
Nicholas Long <nicholas.long@nrel.gov>
2025-09-12 13:51:24 -0600

    fix test

---
+++ building_data_utilities/__init__.py
@@ -0,0 +1,10 @@
+from . import utils  # noqa: F401
+import warnings
+warnings.warn(
+    (
+        "Importing 'building_data_utilities' is deprecated and "
+        "will be removed in a future release."
+    ),
+    DeprecationWarning,
+    stacklevel=2
+)
