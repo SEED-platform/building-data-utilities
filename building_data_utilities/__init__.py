f6fa22c6d6bbee3f3a1e9628a889e3a67373fb62
Nicholas Long <nicholas.long@nrel.gov>
2025-09-12 13:52:53 -0600

    fix precommit

--- building_data_utilities/__init__.py
+++ building_data_utilities/__init__.py
@@ -1,10 +1,7 @@
-from . import utils  # noqa: F401
 import warnings
+
+from . import utils  # noqa: F401
+
 warnings.warn(
-    (
-        "Importing 'building_data_utilities' is deprecated and "
-        "will be removed in a future release."
-    ),
-    DeprecationWarning,
-    stacklevel=2
+    ("Importing 'building_data_utilities' is deprecated and will be removed in a future release."), DeprecationWarning, stacklevel=2
 )
