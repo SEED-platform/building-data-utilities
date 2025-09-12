12a807226f96bc7bb551c9a9e858ec5fdf6c7ef5
kflemin <2205659+kflemin@users.noreply.github.com>
2025-09-12 08:32:06 -0600

    remove old cbl

--- tests/test_building_data_utils_alias.py
+++ tests/test_building_data_utils_alias.py
@@ -1,16 +1,16 @@
 import warnings
 
-# Test that importing cbl_workflow triggers a deprecation warning and works as an alias
+# Test that importing building_data_utilities triggers a deprecation warning and works as an alias
 
 
-def test_cbl_workflow_import_triggers_deprecation():
+def test_bdu_workflow_import_triggers_deprecation():
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter("always")
-        import cbl_workflow
+        import building_data_utilities
 
         assert any(issubclass(warning.category, DeprecationWarning) and "deprecated" in str(warning.message) for warning in w)
         # Check alias works
-        assert hasattr(cbl_workflow, "utils")
-        from cbl_workflow.utils import ubid
+        assert hasattr(building_data_utilities, "utils")
+        from building_data_utilities.utils import ubid
 
         assert hasattr(ubid, "add_ubid_to_geodataframe")
